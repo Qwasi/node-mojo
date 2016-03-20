b21676960d6fe077c36e5fefe29c339370c4afac
Rob Rodriguez <rob.rodriguez@qwasi.com>
2016-03-20 14:49:58 -0700

    #bump v0.0.2-beta.2

--- src/client.js
+++ src/client.js
@@ -25,32 +25,117 @@
 
 'use strict';
 
+import util from 'util';
 import EventEmitter from 'events';
 import request from 'request';
 import promise from 'bluebird';
+import jwt_decode from 'jwt-decode';
+import _ from 'lodash';
+import log from './log';
 
 export default class Client extends EventEmitter {
-    constructor(url, client_id, client_secret) {
+    constructor(options) {
         super();
-        this.url = url;
-        this.client_id = client_id;
-        this.client_secret = client_secret;
+        
+        options = options || {};
+        
+        this.url = options.url;
+        this.client_id = options.client_id;
+        this.client_secret = options.client_secret;
     }
     
     // login returns a user token, using the client_id and client_secret
     // if the api client does not have permission to authenticate users the
     // call will fail.
-    login(username, password, callback) {
-        return new promise(function(resolve, reject) {
-            
+    login(creds, options, callback) {
+        const login_url = util.format('%s/%s', this.url, 'session');
+        
+        if (_.isFunction(creds)) {
+            callback = creds;
+            creds = undefined;
+        }
+        if (_.isFunction(options)) {
+            callback = options;
+            options = undefined;
+        }
+        creds = creds || {};
+        options = options || {};
+        
+        _.defaults(creds, {
+            client_id: this.client_id,
+            client_secret: this.client_secret
+        });
+        
+        if (creds.username && creds.password) {
+            options.grant_type = options.grant_type || 'password';
+        }
+        
+        _.defaults(options, {
+            grant_type: 'client_credentials'
+        });
+        
+        return new promise((resolve, reject) => {
+            request.post({
+                url: login_url,
+                auth: {
+                    username: creds.client_id,
+                    password: creds.client_secret
+                },
+                form: {
+                    grant_type: options.grant_type,
+                    username: creds.username || undefined,
+                    password: creds.password || undefined
+                },
+                json: true
+            }, (err, response, body) => {
+                if (err) {
+                    log('Client %s authenticatication failed: %s.', creds.username || creds.client_id, err);
+                    return reject(err);
+                }
+                if (response.statusCode >= 400) {
+                    let error = body.error_description || body.error;
+                    log('Client %s authenticatication failed: %s.', creds.username || creds.client_id, error);
+                    return reject(new Error(error));
+                }
+                
+                log('Client %s authenticated successfully.', creds.username || creds.client_id);
+                resolve(body);
+            });
         }).nodeify(callback);
     }
     
     // logout deletes the token and invalidates the session associated with
     // that token.
     logout(token, callback) {
-        return new promise(function(resolve, reject) {
+        var logout_url = util.format('%s/%s/%s', this.url, 'session', token);
+        
+        return new promise((resolve, reject) => {
+            let owner = null;
             
+            try {
+                let session = jwt_decode(token);
+                owner = session.owner.name;
+            } catch (e) {
+                log('Failed to decode session token: %s.', e);
+                return reject(e);
+            }
+        
+            request.del({
+                url: logout_url,
+                json: true
+            }, (err, response, body) => {
+                if (err) {
+                    log('Client %s session delete failed: %s.', owner, err);
+                    return reject(err);
+                }
+                if (response.statusCode >= 400) {
+                    let error = body.error_description || body.error;
+                    log('Client %s session delete failed: %s.', owner, error);
+                    return reject(new Error(error));
+                }
+                log('Client %s session deleted.', owner);
+                return resolve(body);
+            });   
         }).nodeify(callback);
     }
 }
